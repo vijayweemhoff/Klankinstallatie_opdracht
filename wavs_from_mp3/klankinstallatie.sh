83e08568344914f5be5d0b7fbb8840a50e258ddd
Vijay Weemhoff <Gebruiker01@MacBook-Pro-van-Vijay-Weemhoff.local>
2015-10-07 18:37:03 +0200

    put all for-loops in 1 for loop. still works

--- wavs_from_mp3/klankinstallatie.sh
+++ wavs_from_mp3/klankinstallatie.sh
@@ -14,31 +14,30 @@ else
 fi
 
 for f in *.wav; do
- sox $f `basename $f .wav`_t.wav trim 5.0 5.0
- sox --norm `basename $f .wav`_t.wav `basename $f .wav`_puur.wav fade h 0:01 0 0:01
- mv -i `basename $f .wav`_puur.wav ${TARGETDIR}
- rm `basename $f .wav`_t.wav
- done
+ # puurgeluid
+  sox $f `basename $f .wav`_t.wav trim 5.0 5.0
+  sox --norm `basename $f .wav`_t.wav `basename $f .wav`_puur.wav fade h 0:01 0 0:01
+  mv -i `basename $f .wav`_puur.wav ${TARGETDIR}
+  rm `basename $f .wav`_t.wav
+ # puurgeluid
+ # overdrive_effect
+  sox $f `basename $f .wav`_o.wav trim 2.0 2.0 overdrive 60 40 vol -0.5
+  sox --norm `basename $f .wav`_o.wav `basename $f .wav`_overdrive.wav fade h 0:01 0 0:01
+  mv -i `basename $f .wav`_overdrive.wav ${TARGETDIR}
+  rm `basename $f .wav`_o.wav
+ # overdrive_effect
+ #otherroom
+  sox $f `basename $f .wav`_l_r.wav trim 4.0 4.0 lowpass -2 300 gain -0.5 pad 0 0.5 reverb 25 
+  sox --norm `basename $f .wav`_l_r.wav `basename $f .wav`_otherroom.wav fade h 0:01 0 0:01
+  mv -i `basename $f .wav`_otherroom.wav ${TARGETDIR}
+  rm `basename $f .wav`_l_r.wav
+ #otherroom 
+ #downpitch
+  sox $f `basename $f .wav`_effect.wav trim 4.0 8.0 pitch -3000 phaser 0.8 0.74 3 0.4 0.5 reverse
+  sox --norm `basename $f .wav`_effect.wav `basename $f .wav`_downpitch.wav fade h 0:01 0 0:01
+  mv -i `basename $f .wav`_downpitch.wav ${TARGETDIR}
+  rm `basename $f .wav`_effect.wav
+ #downpitch
+done 
 
-
-for f in *.wav; do
- sox $f `basename $f .wav`_o.wav trim 2.0 2.0 overdrive 60 40 vol -0.5
- sox --norm `basename $f .wav`_o.wav `basename $f .wav`_overdrive.wav fade h 0:01 0 0:01
- mv -i `basename $f .wav`_overdrive.wav ${TARGETDIR}
- rm `basename $f .wav`_o.wav
- done
-
-for f in *.wav; do
- sox $f `basename $f .wav`_l_r.wav trim 4.0 4.0 lowpass -2 300 gain -0.5 pad 0 0.5 reverb 25 
- sox --norm `basename $f .wav`_l_r.wav `basename $f .wav`_otherroom.wav fade h 0:01 0 0:01
- mv -i `basename $f .wav`_otherroom.wav ${TARGETDIR}
- rm `basename $f .wav`_l_r.wav
-done
-
-for f in *.wav; do
- sox $f `basename $f .wav`_effect.wav trim 4.0 8.0 pitch -3000 phaser 0.8 0.74 3 0.4 0.5 reverse
- sox --norm `basename $f .wav`_effect.wav `basename $f .wav`_downpitch.wav fade h 0:01 0 0:01
- mv -i `basename $f .wav`_downpitch.wav ${TARGETDIR}
- rm `basename $f .wav`_effect.wav
-done
 
