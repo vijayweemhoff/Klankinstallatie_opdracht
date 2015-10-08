4207d9a489e87d3f5f65cfdfed89e42b8c905c41
Vijay Weemhoff <Gebruiker01@MacBook-Pro-van-Vijay-Weemhoff.local>
2015-10-08 11:54:45 +0200

    added question and put back all the for loops

--- wavs_from_mp3/klankinstallatie.sh
+++ wavs_from_mp3/klankinstallatie.sh
@@ -13,31 +13,48 @@ else
   mkdir $TARGETDIR
 fi
 
-for f in *.wav; do
- # puurgeluid
+read -n1 -p "Do you want to process the sounds? [y,n]" doit
+echo ""
+case $doit in  
+  y|Y) echo "allright, lets go!" 
+ for f in *.wav; do
   sox $f `basename $f .wav`_t.wav trim 5.0 5.0
   sox --norm `basename $f .wav`_t.wav `basename $f .wav`_puur.wav fade h 0:01 0 0:01
   mv -i `basename $f .wav`_puur.wav ${TARGETDIR}
   rm `basename $f .wav`_t.wav
- # puurgeluid
- # overdrive_effect
+ done
+ for f in *.wav; do
   sox $f `basename $f .wav`_o.wav trim 2.0 2.0 overdrive 60 40 vol -0.5
   sox --norm `basename $f .wav`_o.wav `basename $f .wav`_overdrive.wav fade h 0:01 0 0:01
   mv -i `basename $f .wav`_overdrive.wav ${TARGETDIR}
   rm `basename $f .wav`_o.wav
- # overdrive_effect
- #otherroom
+ done
+
+ for f in *.wav; do
   sox $f `basename $f .wav`_l_r.wav trim 4.0 4.0 lowpass -2 300 gain -0.5 pad 0 0.5 reverb 25 
   sox --norm `basename $f .wav`_l_r.wav `basename $f .wav`_otherroom.wav fade h 0:01 0 0:01
   mv -i `basename $f .wav`_otherroom.wav ${TARGETDIR}
   rm `basename $f .wav`_l_r.wav
- #otherroom 
- #downpitch
+ done
+
+ for f in *.wav; do
   sox $f `basename $f .wav`_effect.wav trim 4.0 8.0 pitch -3000 phaser 0.8 0.74 3 0.4 0.5 reverse
   sox --norm `basename $f .wav`_effect.wav `basename $f .wav`_downpitch.wav fade h 0:01 0 0:01
   mv -i `basename $f .wav`_downpitch.wav ${TARGETDIR}
   rm `basename $f .wav`_effect.wav
- #downpitch
-done 
+ done ;; 
+  n|N) echo no, okay, its fine ;; 
+  *) echo dont know what you mean ;; 
+esac
+
+
+
+#############################################################################################
+# compositie maken
+# 180 onderdelen 60 seconden 3 sporen alle samples 1 seconden.
+
+
+
+
 
 
