a48dc0fdedc6306407950f4e156c6768197c3954
Vijay Weemhoff <Gebruiker01@MacBook-Pro-van-Vijay-Weemhoff.local>
2015-10-08 12:16:02 +0200

    added questions for making directory and show file

--- wavs_from_mp3/klankinstallatie.sh
+++ wavs_from_mp3/klankinstallatie.sh
@@ -9,9 +9,17 @@ TARGETDIR=klankinstallatie_files
 if [ -d $TARGETDIR ] ; then
   echo "$TARGETDIR directory exists"
 else
-  echo "creating $TARGETDIR directory"
-  mkdir $TARGETDIR
-fi
+  read -n1 -p "No directory yet, shall I make directory for you? [y,n]" doit
+  case $doit in 
+  y|Y)
+  echo ""
+  echo "Yes! creating $TARGETDIR directory"
+  mkdir $TARGETDIR ;;
+  n|N) echo no, okay, its fine ;;
+  *) echo "I dont know what you mean but i won't process the files" ;; 
+  esac
+
+echo "For every .wav file in this directory i'm going to make 4 little files"
 
 read -n1 -p "Do you want to process the sounds? [y,n]" doit
 echo ""
@@ -22,30 +30,29 @@ case $doit in
   sox --norm `basename $f .wav`_t.wav `basename $f .wav`_puur.wav fade h 0:01 0 0:01
   mv -i `basename $f .wav`_puur.wav ${TARGETDIR}
   rm `basename $f .wav`_t.wav
- done
- for f in *.wav; do
+
   sox $f `basename $f .wav`_o.wav trim 2.0 2.0 overdrive 60 40 vol -0.5
   sox --norm `basename $f .wav`_o.wav `basename $f .wav`_overdrive.wav fade h 0:01 0 0:01
   mv -i `basename $f .wav`_overdrive.wav ${TARGETDIR}
   rm `basename $f .wav`_o.wav
- done
 
- for f in *.wav; do
   sox $f `basename $f .wav`_l_r.wav trim 4.0 4.0 lowpass -2 300 gain -0.5 pad 0 0.5 reverb 25 
   sox --norm `basename $f .wav`_l_r.wav `basename $f .wav`_otherroom.wav fade h 0:01 0 0:01
   mv -i `basename $f .wav`_otherroom.wav ${TARGETDIR}
   rm `basename $f .wav`_l_r.wav
- done
 
- for f in *.wav; do
   sox $f `basename $f .wav`_effect.wav trim 4.0 8.0 pitch -3000 phaser 0.8 0.74 3 0.4 0.5 reverse
   sox --norm `basename $f .wav`_effect.wav `basename $f .wav`_downpitch.wav fade h 0:01 0 0:01
   mv -i `basename $f .wav`_downpitch.wav ${TARGETDIR}
   rm `basename $f .wav`_effect.wav
+ echo "working with $f now" 
  done ;; 
   n|N) echo no, okay, its fine ;; 
-  *) echo dont know what you mean ;; 
+  *) echo "I dont know what you mean but i won't process the files" ;; 
 esac
+
+fi
+
 
 
 
